d996490fd4075fef709869ec81c485e8c74715d2
vpomo <pomogalov@gmail.com>
2018-07-29 16:27:13 +0300

    update Oraclize

--- contracts/PriceEthTicker.sol
+++ contracts/PriceEthTicker.sol
@@ -1,41 +1,26 @@
-pragma solidity ^0.4.21;
-//import "installed_contracts/oraclize-api/contracts/usingOraclize.sol";
+pragma solidity ^0.4.24;
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
 contract PriceEthTicker is usingOraclize {
 
-    address owner;
-    string public ETHUSD = "490.05";
+    string public ETHUSD = "465.02";
 
     event LogInfo(string description);
     event LogPriceUpdate(string price);
-    event LogUpdate(address indexed _owner, uint indexed _balance);
+    event LogUpdate(address indexed _sender, uint indexed _balance);
 
     // Constructor
     function PriceEthTicker()
     payable
     public {
-        owner = msg.sender;
-
-        emit LogUpdate(owner, address(this).balance);
-
-        // Replace the next line with your version:
-        OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
-
         oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
+        emit LogUpdate(msg.sender, address(this).balance);
         update();
-    }
-
-    // Fallback function
-    function()
-    public{
-        revert();
     }
 
     function __callback(bytes32 id, string result, bytes proof)
     public {
-        require(msg.sender == oraclize_cbAddress());
-
+        if (msg.sender != oraclize_cbAddress()) revert();
         ETHUSD = result;
         emit LogPriceUpdate(ETHUSD);
         update();
@@ -55,10 +40,34 @@ contract PriceEthTicker is usingOraclize {
             emit LogInfo("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
         } else {
             emit LogInfo("Oraclize query was sent, standing by for the answer..");
-
             // Using XPath to to fetch the right element in the JSON response
             oraclize_query("URL", "json(https://api.coinbase.com/v2/prices/ETH-USD/spot).data.amount");
         }
     }
 
+    function getRate() public view returns (uint result) {
+        result = stringToUint(ETHUSD);
+    }
+
+    function stringToUint(string _amount) internal pure returns (uint result) {
+        bytes memory b = bytes(_amount);
+        uint i;
+        result = 0;
+        bool hasDot = false;
+
+        for (i = 0; i < b.length; i++) {
+            uint c = uint(b[i]);
+
+            if (c >= 48 && c <= 57) {
+                result = result * 10 + (c - 48);
+            }
+
+            if(c == 46){
+                hasDot = true;
+                break;
+            }
+        }
+
+        return result;
+    }
 }
